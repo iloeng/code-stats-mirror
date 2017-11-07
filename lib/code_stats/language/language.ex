5204a993ee8a4054b119d97ea5920de816c92217
Mikko Ahlroth <mikko.ahlroth@gmail.com>
2017-11-07 19:39:18 +0200

    Fix reference to XP schema and reformat file

--- lib/code_stats/language/language.ex
+++ lib/code_stats/language/language.ex
@@ -7,16 +7,16 @@ defmodule CodeStats.Language do
   alias CodeStats.Repo
 
   schema "languages" do
-    field :name, :string
+    field(:name, :string)
 
-    has_many :xps, CodeStats.Language.XP
+    has_many(:xps, CodeStats.XP)
 
     # Either a language has many aliases or it is an alias of some other language,
     # it cannot be both.
     # NOTE: Only 1 level of aliases is supported! That is, you cannot form a chain of
     # aliases.
-    belongs_to :alias_of, __MODULE__
-    has_many :aliases, __MODULE__, foreign_key: :alias_of_id
+    belongs_to(:alias_of, __MODULE__)
+    has_many(:aliases, __MODULE__, foreign_key: :alias_of_id)
 
     timestamps()
   end
@@ -40,29 +40,34 @@ defmodule CodeStats.Language do
 
   Returns ok and the language struct if successful.
   """
-  @spec get_or_create(String.t) :: {:ok, %__MODULE__{}} | {:error, :unknown}
+  @spec get_or_create(String.t()) :: {:ok, %__MODULE__{}} | {:error, :unknown}
   def get_or_create(language_name) do
     # Get-create-get to handle race conditions
-    get_query = from l in __MODULE__,
-      where: fragment("LOWER(?)", l.name) == fragment("LOWER(?)", ^language_name),
-      preload: :alias_of
+    get_query =
+      from(
+        l in __MODULE__,
+        where: fragment("LOWER(?)", l.name) == fragment("LOWER(?)", ^language_name),
+        preload: :alias_of
+      )
 
     case Repo.one(get_query) do
-      %__MODULE__{} = language -> {:ok, language}
+      %__MODULE__{} = language ->
+        {:ok, language}
 
       nil ->
         changeset(%__MODULE__{}, %{"name" => language_name})
         |> put_assoc(:alias_of, nil)
         |> Repo.insert()
         |> case do
-          {:ok, language} -> {:ok, language}
+             {:ok, language} ->
+               {:ok, language}
 
-          {:error, _} ->
-            case Repo.one(get_query) do
-              %__MODULE__{} = language -> {:ok, language}
-              nil -> {:error, :unknown}
-            end
-        end
+             {:error, _} ->
+               case Repo.one(get_query) do
+                 %__MODULE__{} = language -> {:ok, language}
+                 nil -> {:error, :unknown}
+               end
+           end
     end
   end
 end
