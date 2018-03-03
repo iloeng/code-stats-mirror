de5d6265b63ab6f72dc6a1be784c6df72c1c7828
Mikko Ahlroth <mikko.ahlroth@gmail.com>
2018-03-03 20:41:52 +0200

    Fixed machine page JS listeners and added listener for API key regeneration
    Fixes #50

--- assets/js/frontend/machine_page.js
+++ assets/js/frontend/machine_page.js
@@ -1,38 +1,46 @@
+import {wait_for_load} from '../common/utils';
+
 /**
  * Code to execute on machines page. Adds prompt for deleting machines.
  */
 
-function machine_page() {
-  document.addEventListener('DOMContentLoaded', () => {
-    const delete_buttons = document.getElementsByClassName('machine-delete-button');
+async function machine_page() {
+  await wait_for_load();
 
-    for (const button of delete_buttons) {
-      button.onclick = () => {
-        return confirm('Are you sure you wish to delete this machine? You will lose all XP associated with it.');
-      };
-    }
+  const delete_buttons = document.getElementsByClassName('machine-delete-button');
 
-    const deactivate_buttons = document.getElementsByClassName('machine-deactivate-button');
+  for (const button of delete_buttons) {
+    button.onclick = () => {
+      return confirm('Are you sure you wish to delete this machine? You will lose all XP associated with it.');
+    };
+  }
 
-    for (const button of deactivate_buttons) {
-      button.onclick = () => confirm('Are you sure you wish to deactivate this machine? It will be hidden, but can be re-activated.');
-    }
+  const deactivate_buttons = document.getElementsByClassName('machine-deactivate-button');
 
-    const copy_buttons = document.getElementsByClassName('copy-button');
+  for (const button of deactivate_buttons) {
+    button.onclick = () => confirm('Are you sure you wish to deactivate this machine? It will be hidden, but can be re-activated.');
+  }
 
-    for (const button of copy_buttons) {
-      button.onclick = () => {
-        button.parentElement.getElementsByClassName('api-key')[0].select();
-        try {
-          document.execCommand('copy');
-          button.textContent = 'Copied!';
-        }
-        catch (e) {
-          button.textContent = 'Error';
-        }
-      };
-    }
-  });
+  const regenerate_buttons = document.getElementsByClassName('machine-regenerate-button');
+
+  for (const button of regenerate_buttons) {
+    button.onclick = () => confirm('Are you sure you wish to regenerate the API key? The old API key will stop functioning immediately.');
+  }
+
+  const copy_buttons = document.getElementsByClassName('copy-button');
+
+  for (const button of copy_buttons) {
+    button.onclick = () => {
+      button.parentElement.getElementsByClassName('api-key')[0].select();
+      try {
+        document.execCommand('copy');
+        button.textContent = 'Copied!';
+      }
+      catch (e) {
+        button.textContent = 'Error';
+      }
+    };
+  }
 }
 
 export default machine_page;
