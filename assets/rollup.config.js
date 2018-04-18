1e3d42036439b1f5979358cfbac2261cc06564ad
Mikko Ahlroth <mikko.ahlroth@gmail.com>
2018-04-18 20:56:36 +0300

    Bump frontend deps, move babel config to rollup to simplify it

--- assets/rollup.config.js
+++ assets/rollup.config.js
@@ -7,11 +7,37 @@ export default {
   plugins: [
     sourcemaps(),
     resolve(),
-    babel(),
     commonjs({
       namedExports: {
         'deps/phoenix/priv/static/phoenix.js': ['Socket']
       }
+    }),
+    babel({
+      exclude: ['**/node_modules/**', '**/deps/phoenix/**'],
+      presets: [
+        [
+          'env',
+          {
+            targets: {
+              browsers: [
+                'last 2 Chrome versions',
+                'last 2 Edge versions',
+                'last 2 Safari versions',
+                'last 2 Firefox versions',
+                'last 2 and_chr versions',
+                'last 2 and_ff versions',
+                'last 2 ios_saf versions'
+              ]
+            },
+            useBuiltIns: true,
+            modules: false
+          }
+        ]
+      ],
+      plugins: [
+        'external-helpers'
+      ],
+      babelrc: false
     })
   ]
 };
