3685ebf18555ea5138cd8bce2103bd5c29f4d3e2
Mikko Ahlroth <mikko.ahlroth@gmail.com>
2018-06-30 10:19:09 +0300

    Change analytics config example to point to Tilastokeskus

--- config/config.exs
+++ config/config.exs
@@ -42,14 +42,9 @@ config :code_stats,
   # Extra HTML that is injected to every page, right before </body>. Useful for analytics scripts.
   analytics_code: """
   <script>
-    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-    ga('create', 'my-googleanalytics-id', 'auto');
-    ga('send', 'pageview');
+    window.tilastokeskus_url = '/tilastokeskus/track';
   </script>
+  <script src="/tilastokeskus/track.js" async></script>
   """,
 
   # CORS
