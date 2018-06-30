11eeb741025acbf739826e9ede0d84b3205f938c
Mikko Ahlroth <mikko.ahlroth@gmail.com>
2018-07-01 00:14:06 +0300

    Remove "other languages" header when there's no data
    Also switch places of machines and other languages in profile

--- assets/js/frontend/profile/graphs/other-languages.component.js
+++ assets/js/frontend/profile/graphs/other-languages.component.js
@@ -1,12 +1,14 @@
-import {el, list} from 'redom';
+import { el, list } from 'redom';
 import ListableLevelCounterComponent from '../../graphs/listable-level-counter.component';
 
 class OtherLanguagesComponent {
   constructor(data_source) {
     this.list = list('ul', ListableLevelCounterComponent, null, ['li', '', 0, 0]);
 
+    this.title = el('h4', 'Other languages');
+
     this.el = el('section.other-languages', [
-      el('h4', 'Other languages'),
+      this.title,
       this.list
     ]);
 
@@ -25,6 +27,7 @@ class OtherLanguagesComponent {
   _refresh() {
     this._langs = this._dataSource.getOtherLangs();
     this.list.update(this._langs);
+    this.title.style.display = (this._langs.length > 0) ? 'block' : 'none';
   }
 }
 
