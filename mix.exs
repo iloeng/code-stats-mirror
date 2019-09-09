717ca7958938960c1b559cd15d6dfc657535badd
Mikko Ahlroth <mikko.ahlroth@gmail.com>
2019-09-09 18:37:04 +0300

    Bump version to 2.1.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule CodeStats.Mixfile do
   def project do
     [
       app: :code_stats,
-      version: "2.0.12",
+      version: "2.1.0",
       elixir: "~> 1.7",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
