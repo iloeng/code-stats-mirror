8a932d2204299090740359751bb478bb66330cc2
Mikko Ahlroth <mikko@ahlroth.fi>
2020-09-01 19:58:14 +0300

    Add debug function for testing frontend

--- assets/js/frontend/profile_page.js
+++ assets/js/frontend/profile_page.js
@@ -7,6 +7,18 @@ import ProfilePageUpdater from './profile_page_updater';
 
 let updater = null;
 
+function debug_insert_xps(xps, machine) {
+  const now = new Date();
+  updater.newPulse({
+    machine,
+    xps,
+    sent_at: now.toUTCString(),
+    sent_at_local: now.toISOString()
+  });
+}
+
+window.debug_insert_xps = debug_insert_xps;
+
 function profile_page() {
   updater = new ProfilePageUpdater(get_live_update_socket());
 }
